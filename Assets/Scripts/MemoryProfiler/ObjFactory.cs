3a4f8c1931413ee8315d26fc572267c09a2f87d2
Tw1ntrow <gregkerijgoij3pok@gmail.com>
2023-09-06 00:30:08 +0900

    テストパターンを増やす

--- Assets/Scripts/MemoryProfiler/ObjFactory.cs
+++ Assets/Scripts/MemoryProfiler/ObjFactory.cs
@@ -5,7 +5,33 @@ using UnityEngine;
 public class ObjFactory : MonoBehaviour
 {
     List<Texture> _textures = new List<Texture>();
+    List<Vector3> _hugeDataList = new List<Vector3>();
     void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            StartCoroutine(LoadAsyncData());
+        }
+    }
+
+    // オブジェクト生成のテスト
+    void ObjGenerater()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            new GameObject("Obj " + i);
+        }
+    }
+
+    void DataGenerater()
+    {
+        for (int i = 0; i < 1000000; i++)
+        {
+            _hugeDataList.Add(new Vector3(Random.value, Random.value, Random.value));
+        }
+    }
+
+    void TextureGenerater()
     {
         // メモリ使用量を増やすために、テクスチャを10個生成しておく
         for (int i = 0; i < 10; i++)
@@ -14,6 +40,13 @@ public class ObjFactory : MonoBehaviour
             tex.name = "Unused Texure " + i;
             _textures.Add(tex);
         }
+    }
+
+    // 非同期処理のテスト
+    IEnumerator LoadAsyncData()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 5f));
+        Debug.Log("Data Loaded!");
     }
 
 }
