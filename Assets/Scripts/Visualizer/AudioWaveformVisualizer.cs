4a1bddd24a0051ff058b0445703b3a5db27cc06e
Tw1ntrow <gregkerijgoij3pok@gmail.com>
2023-12-03 21:08:28 +0900

    ビジュアライザー

---
+++ Assets/Scripts/Visualizer/AudioWaveformVisualizer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioWaveformVisualizer : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private float[] samples;
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        samples = new float[256];
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+
+        SetupLineRenderer();
+    }
+
+    void Update()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.GetSpectrumData(samples, 0, FFTWindow.BlackmanHarris);
+            DrawWaveform();
+        }
+    }
+
+    void SetupLineRenderer()
+    {
+        lineRenderer.positionCount = samples.Length;
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.02f;
+    }
+
+    void DrawWaveform()
+    {
+        for (int i = 0; i < samples.Length; i++)
+        {
+            Vector3 position = new Vector3(i * 0.1f, samples[i] * 10, 0);
+            lineRenderer.SetPosition(i, position);
+        }
+    }
+}
