b1ecc22b9093ca63888e46f598c88c24ebbc1e4a
Tw1ntrow <gregkerijgoij3pok@gmail.com>
2023-12-13 00:58:30 +0900

    ジャンプ、慣性

--- Assets/Scripts/FPS/FPSController.cs
+++ Assets/Scripts/FPS/FPSController.cs
@@ -7,20 +7,35 @@ namespace GameFunc
         [SerializeField]
         private float speed = 5.0f;
         [SerializeField]
+        private float jumpHeight = 2.0f;
+        [SerializeField]
         private float mouseSensitivity = 100.0f;
         [SerializeField]
         private Transform playerBody;
         [SerializeField]
         private Camera playerCamera;
+        [SerializeField]
+        private float gravity = -9.81f;
 
+        private CharacterController controller;
         private float xRotation = 0f;
+        private Vector3 velocity;
+        private bool isGrounded;
 
         void Start()
         {
+            controller = GetComponent<CharacterController>();
             Cursor.lockState = CursorLockMode.Locked;
         }
 
         void Update()
+        {
+            ProcessMouseLook();
+            ProcessMovement();
+            ProcessGravityAndJump();
+        }
+
+        private void ProcessMouseLook()
         {
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -30,12 +45,31 @@ namespace GameFunc
 
             playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
             playerBody.Rotate(Vector3.up * mouseX);
+        }
 
+        private void ProcessMovement()
+        {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
+            Vector3 move = transform.right * horizontal + transform.forward * vertical;
+            controller.Move(move * speed * Time.deltaTime);
+        }
 
-            Vector3 movement = transform.right * horizontal + transform.forward * vertical;
-            GetComponent<CharacterController>().Move(movement * speed * Time.deltaTime);
+        private void ProcessGravityAndJump()
+        {
+            isGrounded = controller.isGrounded;
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
         }
     }
 }
