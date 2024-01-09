b32dfeca027752459fec6df0f3865ac5a68d884f
Tw1ntrow <gregkerijgoij3pok@gmail.com>
2024-01-10 00:26:17 +0900

    マイク音量検知機能

--- Assets/Scripts/Mic/MicrophoneInput.cs
+++ Assets/Scripts/Mic/MicrophoneInput.cs
@@ -4,29 +4,57 @@ using UnityEngine;
 public class MicrophoneInput : MonoBehaviour
 {
     AudioSource audioSource;
+    [SerializeField]
+    private float sensitivity = 100;
+    [SerializeField]
+    private float loudnessThreshold = 1.0f;
+    [SerializeField]
+    private Vector3 maxScale = new Vector3(2.0f, 2.0f, 2.0f);
+    [SerializeField]
+    private Vector3 originalScale;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        originalScale = transform.localScale;
 
-        // マイクが接続されているか確認
         if (Microphone.devices.Length <= 0)
         {
             Debug.LogWarning("マイクが見つかりません。");
             return;
         }
 
-        // デフォルトのマイクを使用
         string micName = Microphone.devices[0];
         audioSource.clip = Microphone.Start(micName, true, 10, 44100);
         audioSource.loop = true;
 
-        // マイクが準備されるまで待つ
-        // Updateで待ってもいい
-        while (!(Microphone.GetPosition(null) > 0))
-        {
-        }
-
+        while (!(Microphone.GetPosition(null) > 0)) { }
         audioSource.Play();
+    }
+
+    void Update()
+    {
+        float loudness = GetAveragedVolume() * sensitivity;
+
+        if (loudness > loudnessThreshold)
+        {
+            transform.localScale = maxScale;
+        }
+        else
+        {
+            transform.localScale = originalScale;
+        }
+    }
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        audioSource.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        return a / 256;
     }
 }
